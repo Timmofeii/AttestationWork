1278a5478a2b260071eddda76c94453aae7018fb
Tima <timoshaa96@gmail.com>
2024-04-02 00:04:00 +0300

    ADD README.md

--- src/Counter.java
+++ src/Counter.java
@@ -6,13 +6,17 @@ import java.util.stream.Collectors;
 
 public class Counter {
     /**
-     *
      * @param arrWord Принимает массив слов
      * @return Возвращает к-ство слов в этом массиве
      */
-    public int countWords(String[] arrWord){
+    public int countWords(String[] arrWord) {
         return arrWord.length;
     }
+//    public Map<String, Integer> mapWords(String[] arrWords) {
+//        return Arrays.stream(arrWords)
+//                .map(String::toLowerCase)
+//                .collect(Collectors.groupingBy(Function.identity(), Collectors.summingInt(e -> 1)));
+//    }
 
     /**
      * Метод проходит по массиву и сверят по нижнему регистру наличее слова в HashMap, если этого слова нет,
@@ -21,15 +25,15 @@ public class Counter {
      * @param arrWords Принимает массив слов
      * @return Возвращает Map<String,Integer> слово, сколько раз встречается.
      */
-    public  Map<String,Integer>mapWords(String[]arrWords){
-        Map<String,Integer>result= new HashMap<>();
+    public Map<String, Integer> mapWords(String[] arrWords) {
+        Map<String, Integer> result = new HashMap<>();
         for (String word : arrWords) {
-           word= word.toLowerCase();
-            if(result.containsKey(word)) {
-                int count=result.get(word);
-                result.put(word,count+1);
-            }else {
-                result.put(word,1);
+            word = word.toLowerCase();
+            if (result.containsKey(word)) {
+                int count = result.get(word);
+                result.put(word, count + 1);
+            } else {
+                result.put(word, 1);
             }
         }
         return result;
@@ -38,19 +42,20 @@ public class Counter {
 
     /**
      * Метод проходит по ключам Мар в поисках слова с самым большим к-ством
+     *
      * @param maps Принимает Мар
      * @return Возвращает самое длинное слово.
-     *
      */
 
-    public String longerWord( Map<String,Integer> maps){
-        String result="";
+    public String longerWord(Map<String, Integer> maps) {
+        String result = "";
 
-        for(String word: maps.keySet()){
-            if(word.length()>result.length()){
-                result=word;
+        for (String word : maps.keySet()) {
+            if (word.length() > result.length()) {
+                result = word;
             }
         }
-       return result;
+        return result;
     }
 }
+
